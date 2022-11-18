c8efbfde612bce0bdbd52147b81e7bdbeb85fd28
tanaliga100 <jordantanaliga@gmail.com>
2022-11-18 13:44:33 +0800

    add-destruc

--- object-destructure.js
+++ object-destructure.js
@@ -3,14 +3,31 @@
 // the order doesnt matter but the property name must be accurate
 
 const dan = {
-  first: "Jordan",
-  last: "Tanaliga",
+  first: "Lara",
+  last: "Baldesco",
   age: 21,
-  gender: "female",
+  gender: "male",
   habbits: {
     travel: "land travel",
-    music: "play guitar",
+    music: "plays guitar",
   },
+  favColors: ["red", "blue", "green", "yellow"],
 };
-const { gender, habbits } = dan;
-console.log(gender, habbits);
+// const {
+//   gender,
+//   habbits: { travel: adv },
+//   favColors: [first, second, ...others],
+// } = dan;
+
+const personDetails = ({
+  first,
+  last,
+  habbits: { travel, music },
+  favColors,
+}) => {
+  return `${first} ${last} likes ${travel} & also ${music}
+  <ul>${favColors.map((i) => `<li>${i}</li>`)}</ul>
+ `;
+};
+const el = document.querySelector("#result");
+el.innerHTML = personDetails(dan);
