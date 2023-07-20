96a8f4687edd7369c8f28c4b2c4a5e578f780afb
tanaliga100 <jordantanaliga@gmail.com>
2023-07-20 11:07:34 +0800

    recode-fetch

--- fetch.js
+++ fetch.js
@@ -1,28 +1,54 @@
 // fetch api
 
 const url = `https://www.course-api.com/react-tours-project`;
+const uri = `https://randomuser.me/api`;
 
+const state = {
+  user: [],
+  msg: "",
+};
+
+// USING PROMISES
+const fetchData = (endpoint) => {
+  return new Promise((resolve, reject) => {
+    fetch(endpoint)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        resolve({ msg: "success", data });
+      })
+      .catch((error) => {
+        reject({ msg: "failed", error });
+      });
+  });
+};
+
+const endpoint = "https://jsonplaceholder.typicode.com/todos";
+fetchData(endpoint)
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+// USING ASYNC-AWAIT
+const getData = async () => {
+  try {
+    const res = await fetchData();
+    //     console.log(res);
+    state.msg = res.msg;
+    state.user.push(res.data.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+getData();
+
+// console.log("STATE", state);
+
+// BARE MINUMUM
 // fetch(url)
 //   .then((res) => res.json())
 //   .then((data) => console.log(data))
-//   .catch((err) => console.error(err));
-
-const state = {
-  tours: [],
-};
-
-const fetchData = async () => {
-  try {
-    const req = await fetch(url);
-    return req.json();
-  } catch (error) {
-    throw new Error(error);
-  }
-};
-fetchData().then((data) => {
-  if (data) {
-    state.tours.push(data);
-    console.log("state", state);
-  }
-  return;
-});
+//   .catch((err) => console.log(err));
