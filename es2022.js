21bb4c6f1839f4a5f0c2cc3c96e57e4dbfd70766
tanaliga100 <jordantanaliga@gmail.com>
2023-07-20 12:24:32 +0800

    recode-es2022

--- es2022.js
+++ es2022.js
@@ -1,16 +1,21 @@
-const scores = [67, 87, 100, 76, 98];
+const scores = [84, 87, 99, 94, 98];
 
-// at method
+// old way
 const last = scores[scores.length - 1];
 console.log(last);
 
-const last2 = scores.at(2);
+// using "AT" method
+const last2 = scores.at(0);
 console.log(last2);
 
-// top level await
+const str = "jordan100";
+console.log(str.at(0));
 
+// top level await
 const url = "https://www.course-api.com/react-tours-project";
 
 const res = await fetch(url);
 const data = await res.json();
-console.log("data", data);
+console.log(data);
+
+// introducing the uses of default export
