3c970e982b8d57f50c39be43e4121fabddf18eac
tanaliga100 <jordantanaliga@gmail.com>
2023-07-20 11:30:14 +0800

    recode-boundingrect

--- width-height.js
+++ width-height.js
@@ -1,2 +1,16 @@
-console.log("height", window.innerHeight);
-console.log("width", window.innerWidth);
+// WIDTH/HEIGHT = window, any element
+// innerWidth = Width
+// innerHeight = Height
+// getBoundingClientRect() = any element
+
+// console.log("height", window.innerHeight);
+// console.log("width", window.innerWidth);
+
+// TARGET THE BUTTON AND LOG THE DIMENSIONS
+const btn = document.querySelector(".btn");
+const box = document.querySelector(".box");
+
+btn.addEventListener("click", () => {
+  const dimensions = box.getBoundingClientRect();
+  console.log(dimensions);
+});
