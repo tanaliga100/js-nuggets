eefeb21a56f5d8eda1704d4bfa36a2f75e909553
tanaliga100 <jordantanaliga@gmail.com>
2023-07-20 11:16:04 +0800

    recode-fetch-errors

--- fetch-errors.js
+++ fetch-errors.js
@@ -1,17 +1,24 @@
+// HANDLING ERROR IN THE NETWORK | SERVER
+
 const url = "https://www.course-api.com/react-tours-project";
 
 const getTours = async () => {
   try {
     const res = await fetch(url);
     if (!res.ok) {
-      const msg =
-        "There was an error fetching" + res.status + " " + res.statusText;
+      const msg = `There was an error : ${res.status} - ${res.statusText}`;
       throw new Error(msg);
     }
     const data = await res.json();
     console.log("data", data);
   } catch (error) {
-    console.log(error);
+    console.log("CATCH BLOCK", error);
   }
 };
 const btn = document.querySelector(".btn").addEventListener("click", getTours);
+
+// if (!res.ok) {
+//       const msg =
+//         "There was an error fetching" + res.status + " " + res.statusText;
+//       throw new Error(msg);
+//     }
